ae49398b11df334278e7bfad6d75289dec4bd2c9
Hao Jun Chen <haojunchen51@gmail.com>
2024-02-14 12:01:47 -0800

    sp

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -5,6 +5,9 @@ from nltk.tokenize import word_tokenize
 import re
 import sys
 import pickle
+import heapq
+from nltk.util import bigrams
+import time
 
 
 
@@ -82,47 +85,85 @@ def part_one():
 
 
 #return the number of bigram where it fits the format of (given_word, query_word) assuming the word already done the normalization process
-def bigram_frquency(query_word,given_word):
-    freq = 0
-    
-    return freq
+def bigram_frquency(given_word):
+    bigram_freq_dict = {}
+    current_dir = os.getcwd()
+    path = os.path.join(os.path.dirname(current_dir), "yelp")
+    yelp_store_list = os.listdir(path)
+    for yelp_store in yelp_store_list:
+        store_path = path + "/" + yelp_store
+        with open(store_path, 'r') as file:
+            data = json.load(file)
+        reviews = data["Reviews"]
+        #Loop through each review in the yelp _ file
+        for review in reviews:
+            doc = review['Content']
+            tokens = word_tokenize(doc)
+            bigram_list = list(bigrams(tokens))
+            for bigram in bigram_list:
+                if given_word == bigram[0]:
+                    bigram_freq_dict[bigram[1]] = 1 + bigram_freq_dict.get(bigram[1],0)
+    return bigram_freq_dict
 
 
 #bigram(Linear iterpolation smoothing)
 #unigram_percenrage: a hash table that contain all p(w)
 #Given_word, w_i-1
+#lamda = 0.9
 #DF_count, document freq
-def LTS(unigram_percentage, query_word, given_word, tokens_count):
-
-    #c(w_i-1)
-    given_word_freq = unigram_percentage[given_word]
-    #c(w_i-1*w_i)
-    bigram_freq = bigram_frquency(query_word, given_word)
-    #(1-lamda)* c(w_i-1*w_i)/c(w_i-1)
-    mle = 0.1 * (bigram_freq/given_word_freq)
-    parameter = 0.9*(given_word_freq/tokens_count)
-    return mle + parameter
+def LTS(unigram_percentage, bigram_freq_dict,query_word, given_word, tokens_count):
+    if given_word in tokens_count and query_word in bigram_freq_dict and query_word in unigram_percentage:
+        #c(w_i-1)
+        given_word_freq = tokens_count[given_word]
+        #c(w_i-1,w_i)
+        bigram_freq = bigram_freq_dict[query_word]
+        #(1-lamda)* c(w_i-1*w_i)/c(w_i-1)
+        mle = 0.1 * (bigram_freq/given_word_freq)
+        parameter = 0.9 * unigram_percentage[query_word]
+        return mle + parameter
+    else:
+        return 0
 
 #bigram(aboslute dicounts smoothing)
+#unigram_percenrage: a hash table that contain all p(w)
+#Given_word, w_i-1
+#DF_count, document freq
 def ADS(unigram_percentage, query_word, given_word, tokens_count):
     pass
 def main():
+    with open("tokens_count.pkl", "rb") as f:
+        bigram_freq_dict = pickle.load(f)
     with open("tokens_count.pkl", "rb") as f:
         tokens_count = pickle.load(f)
     with open("total_count.txt", "r") as f:
         total_count = int(f.read())
     with open("unigram_percentage.pkl", "rb") as f:
         unigram_percentage = pickle.load(f)
-    #find the top 10 word corresponding to LTS
-    given_word = "good"
-    top_ten_LTS_value = []
-    top_ten_LTS_token = []
-    
-    for token in tokens_count:
-        LTS_value = LTS(unigram_percentage, token, given_word, tokens_count)
-        if len(top_ten_LTS_value) < 10:
-            top_ten_LTS_token.append(token)
-        else:
+    with open("output.txt", "w") as f:
+        sys.stdout = f
+        print(tokens_count)
+    # #find the top 10 word corresponding to LTS
+    # given_word = "good"
+    # #Finding the top 10 most frequent word given the word "good"
+    # top_ten_LTS_tokens = []  
+    # for token in tokens_count:
+    #     LTS_value = LTS(unigram_percentage, bigram_freq_dict,token, given_word, tokens_count)
+    #     print(f"Token: {token},LTS:{LTS_value}")
+    #     # If the length of top_ten_LTS_tokens is less than 10, simply add the token
+    #     if len(top_ten_LTS_tokens) < 10:
+    #         heapq.heappush(top_ten_LTS_tokens, (LTS_value, token))
+    #     else:
+    #         # If the current LTS value is greater than the smallest LTS value in top_ten_LTS_tokens, replace it
+    #         if LTS_value > top_ten_LTS_tokens[0][0]:
+    #             heapq.heappop(top_ten_LTS_tokens)  # Remove the smallest LTS value
+    #             heapq.heappush(top_ten_LTS_tokens, (LTS_value, token))  # Push the new LTS value and token
+
+    # # Once the loop is done, the top_ten_LTS_tokens list will contain the top 10 tokens with the highest LTS values
+    # top_ten_LTS_tokens = [(value, token) for value, token in sorted(top_ten_LTS_tokens, reverse=True)]
+    # with open("output.txt", "w") as f:
+    #     sys.stdout = f
+    #     print(top_ten_LTS_tokens)
+  
             
     
     
--- HW2/test.py
+++ HW2/test.py
@@ -7,17 +7,21 @@ current_dir = os.getcwd()
 path = os.path.join(os.path.dirname(current_dir), "yelp")
 yelp_store_list = os.listdir(path)
 given_word = "good"
-
-freq = 0
+query_word = "but"
+freq = []
+current_dir = os.getcwd()
+path = os.path.join(os.path.dirname(current_dir), "yelp")
+yelp_store_list = os.listdir(path)
 for yelp_store in yelp_store_list:
     store_path = path + "/" + yelp_store
     with open(store_path, 'r') as file:
         data = json.load(file)
     reviews = data["Reviews"]
+    #Loop through each review in the yelp _ file
     for review in reviews:
         doc = review['Content']
         tokens = word_tokenize(doc)
         bigram_list = list(bigrams(tokens))
         for bigram in bigram_list:
-            if given_word in bigram:
-                print(bigram)
+            if given_word == bigram[0] and query_word == bigram[1]:
+                freq += 1
